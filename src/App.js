d4436652b92386bcf6734d69338575ae38c3a254
Tarcísio F. S. Teles <tfst@poli.br / tfst@ecomp.poli.br>
2021-12-08 12:40:44 -0300

    feat: basic initial screen layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import LightningLogo from "./shared/LightningLogo.js";
+
+function LaunchButton() {
+    return (
+        <div className="launch-game">
+            <input type="button" value="Praticar React" />
+            <img src="assets/images/next.png" alt="fast forward icon" />
+        </div>
+    );
+}
+
+
 export default function App() {
     return (
-        <>
-            app
-        </>
+        <div className="main-container">
+            <LightningLogo />
+            <LaunchButton />
+        </div>
     );
 }
---
+++ src/shared/LightningLogo.js
@@ -0,0 +1,7 @@
+export default function LightningLogo() {
+    return (
+        <div className="big-logo">
+            <img src="assets/images/logo.png" alt="lightning logo" />
+        </div>
+    );
+}
