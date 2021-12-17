9c0c10497066a26b6ef1de4e96d5b8df8f917b44
Guga <contato.gustavobarbosa@outlook.com>
2021-12-17 15:12:48 -0300

    Preparando revisão de código

--- src/components/InGamePage/Congrats.js
+++
@@ -1,8 +0,0 @@
-export default function Congrats() {
-  return (
-    <div className="congratulations">
-      <h6>PARABÉNS!</h6>
-      <p>Você não esqueceu nenhum flashcard!</p>
-    </div>
-  );
-}
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom';
-import App from './App.js';
-
-import "./assets/css/reset.css";
-import "./assets/css/styles.css";
-
-ReactDOM.render(<App />, document.querySelector(".root"));
